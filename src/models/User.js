38cfb20dbd4a93a9b808d4ee0984662dd58d0173
faelpatrick <faelpatrick@gmail.com>
2022-01-05 18:50:47 -0300

    Learn more mongoose  Schema and Model

--- src/models/User.js
+++ src/models/User.js
@@ -4,9 +4,7 @@ const userSchema = new mongoose.Schema(
         email: {
             type: String,
             required: true,
-            index: {
-                unique: true
-            }
+            index: { unique: true }
         },
         password: {
             type: String,
